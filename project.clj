9848dd3427840856b5e62d274610f68ef5d0dbd8
jstaffans <johannes.staffans@gmail.com>
2016-11-10 18:32:04 +0100

    Fix version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-kinesis-worker "0.l.0"
+(defproject clj-kinesis-worker "0.1.0"
   :author "Johannes Staffans"
   :description "Wrapper for the Amazon Kinesis Client library"
   :url "https://github.com/jstaffans/clj-kinesis-worker"
